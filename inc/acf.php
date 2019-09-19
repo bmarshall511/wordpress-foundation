990f6a5aff3717d11140f610b9fec53353b64190
Ben Marshall <bmarshall511@gmail.com>
2019-09-19 14:17:42 -0500

    updates

--- inc/acf.php
+++ inc/acf.php
@@ -203,3 +203,14 @@ function foundation_get_configuration_options() {
 
   return $json['fields'];
 }
+
+function foundation_acf_load_options( $paths ) {
+  $paths = array( get_template_directory() . '/acf-json' );
+
+  if( is_child_theme() ) {
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+  }
+
+  return $paths;
+}
+add_filter( 'acf/settings/load_json', 'foundation_acf_load_options' );
