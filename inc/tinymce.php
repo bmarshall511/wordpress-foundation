3d22c460c3fa61b2168fdffa92c27a846e7cea5c
Ben Marshall <bmarshall511@gmail.com>
2019-11-20 09:55:08 -0600

    updates

--- inc/tinymce.php
+++ inc/tinymce.php
@@ -19,43 +19,47 @@ class Foundation_TinyMCE {
       add_filter( 'mce_buttons_2', array( $this, 'mce_buttons_2' ) );
       add_filter( 'tiny_mce_before_init', array( $this, 'tiny_mce_before_init' ) );
 
-      // TinyMCS CSS
-      $stylesheets = array();
+      add_action( 'admin_init', array( $this, 'editor_styles' ) );
+    }
+  }
 
-      if ( function_exists( 'get_field' ) ) {
-        // Foundation libraries
-        $load_foundation_editor_styles = get_field( 'foundation_editor_load_foundation', 'option' );
-        if ( $load_foundation_editor_styles ) {
-          $foundation_files = foundation_get_autoloaded_files( 'css' );
-          if ( $foundation_files ) {
-            $stylesheets = array_merge( $stylesheets, $foundation_files );
-          }
-        }
+  public function editor_styles() {
+    // TinyMCS CSS
+    $stylesheets = array();
 
-        // Theme libraries
-        $load_theme_editor_styles = get_field( 'foundation_editor_load_theme', 'option' );
-        if ( $load_theme_editor_styles ) {
-          $theme_files = foundation_get_theme_autoloaded_files( 'css' );
-          if ( $theme_files ) {
-            $stylesheets = array_merge( $stylesheets, $theme_files );
-          }
-        }
-
-        // CSS Files
-        if ( have_rows( 'foundation_editor_load_css', 'option' ) ) {
-          while ( have_rows( 'foundation_editor_load_css', 'option' ) ) { the_row();
-            $css = get_sub_field( 'css_path' );
-            if ( $css ) {
-              $stylesheets[] = $css;
-            }
-          }
+    if ( function_exists( 'get_field' ) ) {
+      // Foundation libraries
+      $load_foundation_editor_styles = get_field( 'foundation_editor_load_foundation', 'option' );
+      if ( $load_foundation_editor_styles ) {
+        $foundation_files = foundation_get_autoloaded_files( 'css' );
+        if ( $foundation_files ) {
+          $stylesheets = array_merge( $stylesheets, $foundation_files );
         }
       }
 
-      $stylesheets[] = FOUNDATION_ASSETS . '/css/wordpress/tinymce-editor.css';
+      // Theme libraries
+      $load_theme_editor_styles = get_field( 'foundation_editor_load_theme', 'option' );
+      if ( $load_theme_editor_styles ) {
+        $theme_files = foundation_get_theme_autoloaded_files( 'css' );
+        if ( $theme_files ) {
+          $stylesheets = array_merge( $stylesheets, $theme_files );
+        }
+      }
 
-      add_editor_style( $stylesheets );
+      // CSS Files
+      if ( have_rows( 'foundation_editor_load_css', 'option' ) ) {
+        while ( have_rows( 'foundation_editor_load_css', 'option' ) ) { the_row();
+          $css = get_sub_field( 'css_path' );
+          if ( $css ) {
+            $stylesheets[] = $css;
+          }
+        }
+      }
     }
+
+    $stylesheets[] = FOUNDATION_ASSETS . '/css/wordpress/tinymce-editor.css';
+
+    add_editor_style( $stylesheets );
   }
 
   public function mce_buttons_2( $buttons ) {
